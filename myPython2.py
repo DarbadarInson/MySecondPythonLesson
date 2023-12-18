c0d258358432ed111e6d0fac65d42706a516c52a
Ozodbek <devanabuildmuber@gmail.com>
2023-12-18 12:08:11 -0500

    build Second Python tutorial

---
+++ myPython2.py
@@ -0,0 +1,53 @@
+"""
+Yes dears, 
+I will not write more comments after this. 
+I can only comment that I love Python... :)
+Sorry :(
+"""
+#Band name Generator
+#Variabels
+
+print("Welcome to the band name generator.")
+
+city = input("Which city did you grow up in?\n")
+
+pet = input("What is the name of a pet?\n")
+
+print("Your band name could be " + city + ", " + pet)
+
+#Variabels
+username = "Ozodbek"
+print(username)
+
+name = input("What is your name? \n")
+print("Hi,dear " + name + "!")
+
+a = "John"
+print(a)
+
+a = "Julia"
+print(a)
+
+name = input("What is your name? ")
+length = len(name)
+print(length)
+
+a = input("a: ")
+b = input("b: ")
+
+print("a = " + a)
+print("b = " + b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
